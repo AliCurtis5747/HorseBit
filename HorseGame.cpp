cd0dc187909bd8998fb8b39f86623ca1937104c1
AliCurtis5747 <89867025+AliCurtis5747@users.noreply.github.com>
2024-01-21 18:50:37 -0500

    Update HorseGame.cpp
    Added a while loop with an exit condition. The user can now play the games as many times as they want.

--- HorseGame.cpp
+++ HorseGame.cpp
@@ -2,39 +2,50 @@
 
 #include <iostream>
 #include <stdio.h> 
-#include <math.h> 
-
+#include <math.h>
+#include <string>
 
 #define PI 3.14159265
+
 int main()
 {   
-
     int horse1, horse2;
     double angle1 = 0;
-    double angle2 = 0; 
-    std::cout << "Enter the speed of Horse1\n";
-    std::cin >> horse1;
-    std::cout << "Enter the speed of Horse2\n";
-    std::cin >> horse2;
+    double angle2 = 0;
+    std::string answer; 
 
-    std::cout << "Enter the starting angle of Horse1\n";
-    std::cin >> angle1; 
-    std::cout << "Enter the starting angle of Horse2\n";
-    std::cin >> angle2;
+    while (true) {
+        std::cout << "Enter the speed of Horse1\n";
+        std::cin >> horse1;
+        std::cout << "Enter the speed of Horse2\n";
+        std::cin >> horse2;
 
-    double horse1x = horse1 * cos(angle1 * PI / 180);
-    printf("Horse1 is going %f m/s along the x axis\n", horse1x);
-    double horse2x = horse2 * cos(angle2 * PI / 180);
-    printf("Horse2 is going %f m/s along the x axis\n", horse2x);
+        std::cout << "Enter the starting angle of Horse1\n";
+        std::cin >> angle1;
+        std::cout << "Enter the starting angle of Horse2\n";
+        std::cin >> angle2;
 
-    if (horse2x > horse1x) {
-        printf("Horse2 is the winner!");
-    }
-    else if (horse2x < horse1x) {
-        printf("Horse1 is the winner!");
-    }
-    else if (horse2x == horse1x) {
-        printf("The horses are tied!");
+        double horse1x = horse1 * cos(angle1 * PI / 180);
+        printf("Horse1 is going %f m/s along the x axis\n", horse1x);
+        double horse2x = horse2 * cos(angle2 * PI / 180);
+        printf("Horse2 is going %f m/s along the x axis\n", horse2x);
+
+        if (horse2x > horse1x) {
+            printf("Horse2 is the winner! ");
+        }
+        else if (horse2x < horse1x) {
+            printf("Horse1 is the winner! ");
+        }
+        else if (horse2x == horse1x) {
+            printf("The horses are tied! ");
+        }
+
+        std::cout <<"Would you like to play again? y/n \n";
+        std::cin >> answer;
+        std::cout << "\n";
+        if (answer == "n") {
+            exit(0);
+        }
     }
 
     
